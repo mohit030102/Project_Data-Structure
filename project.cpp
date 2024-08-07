acc3f0b33e531ce648166f0481dde62701ef6841
mohit030102 <161075444+mohit030102@users.noreply.github.com>
2024-08-07 14:48:58 -0400

    Header Comment has been added

--- project.cpp
+++ project.cpp
@@ -1,20 +1,11 @@
-// project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+* File name: project.cpp
+* Project: SENG1050 - project
+* Programmer: Mohit Chodavdeeya (8963902)
+*			  Raj Rana (8960902)
+* Date of version created: 2024-08-07
+* Description: The below presented program calculate the different statistics about the	items to be delivered 
+*			   through courier company using Hash Table and Tree Structure.
+*/
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
