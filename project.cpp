0270f70c90b5d50f894e00839590683596d9a9a2
rajrana1012 <rrana0903@conestogac.on,ca>
2024-08-08 01:41:59 -0400

    Inserting header files and impliment structure

--- project.cpp
+++ project.cpp
@@ -8,4 +8,46 @@
 *			   through courier company using Hash Table and Tree Structure.
 */
 
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <limits>
+#pragma warning (dissable : 4996) 
 
+using namespace std;
+
+struct Parcel
+{
+	char* destination;
+	int weight;
+	float valuation;
+	Parcel* left;
+	Parcel* right;
+	int height;
+};
+
+struct AVLTree
+{
+	Parcel* root;
+
+};
+
+struct HashTable
+{
+	AVLTree* table[127];
+};
+
+unsigned long djb2(const char* str)
+{
+	unsigned long hash = 5381;
+	int c;
+	while ((c=*str++))
+		hash = ((hash << 5) + hash) + c; //hash * 33 + c return hash % 127
+}
+
+int max(int a, int b)
+{
+	return (a > b) ? a : b;
+}
